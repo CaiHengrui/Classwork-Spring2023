3014bf46e78e26b4af419173fb4bba56af7d7b7c
CaiHengrui <hc341@duke.edu>
2023-02-06 17:33:13 -0500

    Use decorator---one function but check multiple times

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -3,36 +3,55 @@
 # 2. Act
 # 3. Assert
 
-def test_HDL_analysis( ):
+
+#decorator---one function but check multiple times
+import pytest
+
+@pytest.mark.parametrize("HDL_input,expected",
+[(65,"Normal"),
+ (45,"Borderline low"),
+ (20,"low")
+ ])
+
+def test_HDL_analysis(HDL_input,expected):
     from blood_calculator import HDL_analysis
     # 1. Arrange
-    HDL_input = 65
     # 2. Act
     answer = HDL_analysis(HDL_input)
     # 3. Assert
-    assert answer == "Normal"
+    assert answer == expected
+
+# def test_HDL_analysis():
+    # from blood_calculator import HDL_analysis
+    # # 1. Arrange
+    # HDL_input = 65
+    # expected = "Normal"
+    # # 2. Act
+    # answer = HDL_analysis(HDL_input)
+    # # 3. Assert
+    # assert answer == "Normal"
     
-    HDL_input = 45
-    answer = HDL_analysis(HDL_input)
-    assert answer == "Borderline low"
-    # not recommended
+    # HDL_input = 45
+    # answer = HDL_analysis(HDL_input)
+    # assert answer == "Borderline low"
+    # # not recommended
     
     
     
-def test_HDL_analysis_Borderline_Low():
-    from blood_calculator import HDL_analysis
-    # 1. Arrange
-    HDL_input = 45
-    # 2. Act
-    answer = HDL_analysis(HDL_input)
-    # 3. Assert
-    assert answer == "Borderline low"
+# def test_HDL_analysis_Borderline_Low():
+    # from blood_calculator import HDL_analysis
+    # # 1. Arrange
+    # HDL_input = 45
+    # # 2. Act
+    # answer = HDL_analysis(HDL_input)
+    # # 3. Assert
+    # assert answer == "Borderline low"
     
-def test_HDL_analysis_Low():
-    from blood_calculator import HDL_analysis
-    # 1. Arrange
-    HDL_input = 5
-    # 2. Act
-    answer = HDL_analysis(HDL_input)
-    # 3. Assert
-    assert answer == "low"
+# def test_HDL_analysis_Low():
+    # from blood_calculator import HDL_analysis
+    # # 1. Arrange
+    # HDL_input = 5
+    # # 2. Act
+    # answer = HDL_analysis(HDL_input)
+    # # 3. Assert
+    # assert answer == "low"
