8f77e9ea808f58b8f1ad7e5adeb22fc5fc934364
CaiHengrui <hc341@duke.edu>
2023-02-05 23:40:21 -0500

    how to use enumerate command

--- database2.py
+++ database2.py
@@ -1,5 +1,5 @@
 def create_patient_entry(patient_name, patient_mrn, patient_age):
-    new_patient = [patient_name, patient_mrn, patient_age]
+    new_patient = [patient_name, patient_mrn, patient_age,[]]
     return new_patient
     
     
@@ -9,19 +9,34 @@ def main_driver():
     db.append(create_patient_entry("Bob Boyles", 2, 45))
     db.append(create_patient_entry("Chris Chou", 3, 52))
     print(db)
-    mrn_to_find = 4
-    print("Get patient Ann")
-    found_patient = get_patient_entry(db,mrn_to_find)
-    if found_patient is False:
-        print("Patient mrn {} not found".format(mrn_to_find))
-    else:
-        print(found_patient)
+    add_test_to_patient(db, 1, "HDL", 120)
+    add_test_to_patient(db, 2, "LDL", 100)
+    add_test_to_patient(db, 2, "HDL", 99)
+    room_numbers = ["103", "232", "333"]
+    print(db)
+    print_directory(db, room_numbers)
  
+
+def print_directory(db, room_numbers):
+    for i, patient in enumerate(db):
+        print("Patient {} is in room {}".format(patient[0], room_numbers[i]))
+   
+
+
 def get_patient_entry(db, mrn_to_find):
     for patient in db:
         if patient[1] == mrn_to_find:
             return patient
     return False
+
+
+def add_test_to_patient(db, mrn_to_find, test_name, test_value):
+    patient = get_patient_entry(db, mrn_to_find)
+    if patient == False:
+        print("Bad entry")
+    else:
+        patient[3].append([test_name, test_value])#3代表第四空
+    return   #表示结束 没意义
 
 
 if __name__ == "__main__":
