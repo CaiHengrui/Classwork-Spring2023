759cff8f54386e17eb531932b8631ce6cdb64403
CaiHengrui <hc341@duke.edu>
2023-02-16 20:50:46 -0500

    Add test about calculating y-intercept and y on the line

--- test_calculate_y.py
+++ test_calculate_y.py
@@ -33,3 +33,31 @@ def test_calculate_slope(x1,x2,y1,y2,expected     ):
     from calculate_y import calculate_slope
     answer = calculate_slope(x1,x2,y1,y2)
     assert answer == expected
+   
+
+   
+@pytest.mark.parametrize("slope,x1,y1,expected",
+[(1,0,0,0),
+(0,0,0,0),
+
+])   
+    
+def test_calculate_y_intercept(slope,x1,y1,expected ):
+    from calculate_y import calculate_y_intercept
+    answer = calculate_y_intercept(slope,x1,y1)
+    assert answer == expected
+
+
+
+@pytest.mark.parametrize("slope, x, y_inter,expected",
+[(1,2,0,2),
+(0,3,0,0)
+
+])
+
+
+
+def test_calculate_y_on_line(slope, x, y_inter,expected):
+    from calculate_y import calculate_y_on_line
+    answer = calculate_y_on_line(slope, x, y_inter)
+    assert answer == expected
