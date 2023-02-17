d57930c5449e87dc00b3594afd432227eddfbea8
CaiHengrui <hc341@duke.edu>
2023-02-17 18:37:57 -0500

    Grouping all of the variables as part of the CLass

--- database_class.py
+++ database_class.py
@@ -2,7 +2,7 @@ class Patient:
 
     def __init__(self):
         self.first_name = ""
-        self.lsat_name = ""
+        self.lsat_name = ""    #like a container
         self.mrn = 0
         self.age = 0
         self.tests = []
@@ -13,6 +13,13 @@ def main():
     second_patient = Patient()
     print(new_patient)
     print(second_patient)
+    new_patient.first_name = "David"
+    new_patient.last_name = "Ward"
+    new_patient.tests.append(("HDL", 100))
+    print(new_patient.first_name)
+    print(new_patient.last_name)
+    print(new_patient.tests)
+    print(second_patient.first_name)#这个打不出来因为本来self里面就是空的，别的不会改变self里的设置
     
     
 if __name__ == "__main__":
@@ -20,11 +27,10 @@ if __name__ == "__main__":
     
     
 # $ python database_class.py
-# <__main__.Patient object at 0x00000286D8CAFFD0> 
-# print out the fact and give the memory location
+# <__main__.Patient object at 0x0000024BDBE5FFD0>
+# <__main__.Patient object at 0x0000024BDBE5FEE0>
+# David
+# Ward
 
-# $ python database_class.py
-# <__main__.Patient object at 0x000001C1C11CFFD0>
-# <__main__.Patient object at 0x000001C1C11CFEB0>
+#Grouping all of the variables as part of the Class
 
- 
