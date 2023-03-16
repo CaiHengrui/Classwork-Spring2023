f4b20353e3eea2a747e145e9c7614c543456d95d
CaiHengrui <hc341@duke.edu>
2023-03-16 18:37:25 -0400

    Added /add_test route to receive new test data and store with patient

--- health_db_client.py
+++ health_db_client.py
@@ -7,3 +7,14 @@ r = requests.post(server + "/new_patient", json=patient)
 print(r.status_code)
 print(r.text)
 
+
+patient = {"mrn": 2, "name": "John", "blood_type": "A+"}
+r = requests.post(server + "/new_patient", json=patient)
+print(r.status_code)
+print(r.text)
+
+
+data = {"id": 1, "test_name": "HDL", "test_result": 150}
+r = requests.post(server + "/add_test", json=data)
+print(r.status_code)
+print(r.text)
